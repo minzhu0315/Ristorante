379a362154bf0888d73ee17284d579cd2c5d4bf8
Min Zhu <minzhu@andrew.cmu.edu>
2020-03-11 17:56:25 -0400

    implement home page and router to seperate pages

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './MenuComponent'
-import Dishdetail from './DishdetailComponent'
+import Menu from './MenuComponent';
+import Dishdetail from './DishdetailComponent';
+import Home from './HomeComponent';
+import Header from './HeaderComponent';
 import { DISHES } from '../shared/dishes';
+import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom'; 
 
 class Main extends Component {
 
@@ -11,27 +14,25 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
     };
   }
 
-    onDishSelect(dishId) {
-        // can't access this.state.selectDish = dish directly
-        this.setState({ selectedDish: dishId});
-    }
-
   render() {
+
+    const HomePage = () => {
+        return (
+            <Home />
+        )
+    }
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} 
-            onClick={(dishId) => this.onDishSelect(dishId)} />
-        <Dishdetail 
-            selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0] } /> 
+        <Header />
+        <Switch>
+            <Route path="/home" component={HomePage} />
+            <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} />} />
+            <Redirect to="/home" />
+        </Switch>
+        <Footer />
       </div>
     );
   }
